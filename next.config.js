1526c3c6314fd201f456c5af7b7b7b7d39776924
bandit pro <faisalmuarif10@gmail.com>
2025-09-29 19:38:51 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Kamu bisa tambahkan pengaturan env, rewrites, dll sesuai kebutuhan
+};
+
+module.exports = nextConfig;
